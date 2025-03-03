b5ba943dde9cc94bde1b0446494277b9c28ee37d
AlAmeen-11 <al4565292@gmail.com>
2025-03-03 13:06:41 +0530

    creating  checkout page

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,1 +1,28 @@
 export let cart = [];
+
+export function addItemToCart(productId){
+    let matchingItem;
+    cart.forEach((cartItem)=>{
+      if(productId === cartItem.productId){
+        matchingItem = cartItem;
+      }
+    })
+    if(matchingItem){
+      matchingItem.quantity += 1;
+    }
+    else{
+      cart.push({
+        productId : productId,
+        quantity:1
+      })
+    }
+  }
+  
+  export function updateCartNumber(){
+    let cartQuantity = 0;
+        
+    cart.forEach((cartItem) =>{
+        cartQuantity += cartItem.quantity;
+    })
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+  }
